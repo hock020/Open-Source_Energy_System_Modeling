211e883b176bd12faebfbe9cddc6ff9dfc79b070
Gregor Fischer <bajj17+github@pm.me>
2026-03-22 23:12:04 +0100

    organise imports

--- tests/test_Programm.py
+++ tests/test_Programm.py
@@ -1,4 +1,5 @@
-import src.Programm                          
+import src.Programm
+
 
 def test_add_transaction():
     assert src.Programm.add_transaction("testing.txt","30/1/2025", "Checking", "Savings", "400", "Transfer") == "30/1/2025;Checking;Savings;400;Transfer;\n"
