b0b5e813979c7e912e2a85792865ab06803732df
Gregor Fischer <bajj17+github@pm.me>
2026-03-22 22:59:25 +0100

    Implement Pytests

--- src/Programm.py
+++ src/Programm.py
@@ -1,14 +1,16 @@
-
 filename = "transactions.txt"
 
-def add_transaction(date, outgoing, incoming, amount, category):
-    with open(filename, "a", encoding="utf=8") as f:
-        f.write(date + ";" + outgoing + ";" + incoming + ";" + amount + ";" + category + ";\n")
+def add_transaction(file,date, outgoing, incoming, amount, category):
+    entry_string = date + ";" + outgoing + ";" + incoming + ";" + amount + ";" + category + ";\n"
+    with open(file, "a", encoding="utf=8") as f:
+        f.write(entry_string)
         #adds an entry to the list seperating the columns by semicolons and the entries by lines.
+    return entry_string
 
-def list_transactions():
-    with open(filename, "r", encoding="utf=8") as f:
+def list_transactions(file):
+    with open(file, "r", encoding="utf=8") as f:
         print(f.read())
+    return 1
         #lists all the entries in the list
 
 def ask_entry(): #Getting data from user and adding it to the list
@@ -20,7 +22,7 @@ def ask_entry(): #Getting data from user and adding it to the list
     incoming = input("What's the incoming account? ")
     amount = input("How much is being transfered? ")
     category = input("What category? ")
-    add_transaction(date, outgoing, incoming, amount, category)
+    add_transaction(filename,date, outgoing, incoming, amount, category)
     
 def menu_selection():
     print("What do you want to do? \n" \
@@ -37,4 +39,5 @@ def menu_selection():
     else:
         print("Goodbye")
         
-menu_selection()
+if __name__ == '__main__':
+    menu_selection()
