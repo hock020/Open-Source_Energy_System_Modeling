ed3dcb9cd5b3fcbe7d8037a3b4870b852d7d5d2a
CMHuber <dein@email.de>
2026-04-10 14:29:15 +0200

    Fehler in Test eingebaut.

--- tests/test_Programm.py
+++ tests/test_Programm.py
@@ -2,5 +2,5 @@ import src.Programm
 
 
 def test_add_transaction():
-    assert src.Programm.add_transaction("testing.txt","30/1/2025", "Checking", "Savings", "400", "Transfer") == "30/1/2025;Checking;Savings;400;Transfer;\n"
+    assert src.Programm.add_transaction("testing.txt","30/1/2025", "Checking", "Savings", "400", "Transfer") == "30/12025;Checking;Savings;400;Transfer;\n"
 
